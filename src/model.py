4fb90c8fc47b7df4db70a75a04f24d7a8c197d58
Matthew Fournier <matthew.fournier@360pi.com>
2015-06-16 22:07:27 -0400

    add feature scaling to model
    Issue: master

--- src/model.py
+++ src/model.py
@@ -1,10 +1,12 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 def logistic():
     steps = [
-        ("logistic", LogisticRegression)    
+        ("scaler", StandardScaler()),
+        ("logistic", LogisticRegression())    
     ]
     pipe = Pipeline(steps=steps)
     return pipe
