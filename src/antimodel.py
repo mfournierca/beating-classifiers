829f3d980c6f00eebe302bc5f35ffe82d5600566
Matthew Fournier <fournier.matthew@gmail.com>
2015-06-22 22:24:51 -0400

    use training data to build feature specs and constraints
    Issue: master

--- src/antimodel.py
+++ src/antimodel.py
@@ -9,16 +9,31 @@ from sklearn.linear_model import LogisticRegression
 from src import data
 
 
-df = data.load_spambase()
+# In the final version of this module we won't be tightly coupled to the 
+# features of the model that we are attacking. The decoupling would require
+# a bijection between feature vectors and objects (e.g. email messages) which
+# would make matching feature column names and order irrelevant. 
+# We don't have that bijection yet so we rely on our features having the
+# same order and names as the model we attack.
+xtrain, xtest, ytrain, test = data.split_spambase()
 SPAMBASE_FEATURE_SPECS = [
     {
         "name": c,
-        "type": int if type(df[c].ix[0]) == np.int64 else float,
-        "max": max(df[c]),
-        "min": min(df[c])
+        "type": int if type(xtrain[c].ix[0]) == np.int64 else float,
+        "max": max(xtrain[c]),
+        "min": min(xtrain[c])
     }
-    for c in df.columns if c != "spam"
+    for c in xtrain.columns if c != "spam"
 ]
+
+# the constraints depend on the order of the feature specs
+SPAMBASE_CONSTRAINTS = []
+assert xtrain.columns[0] == "capital_run_length_average"
+SPAMBASE_CONSTRAINTS.append({
+    "name": "capital_run_length_average_gt_10",
+    "type": "ineq", 
+    "fun": lambda x: x[0] - 10
+})
 
 
 class AntiModel(object):
