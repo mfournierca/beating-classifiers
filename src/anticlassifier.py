0a193e8f1b307ea4e48d154cedd8383fb5182762
Matthew Fournier <fournier.matthew@gmail.com>
2015-07-12 22:22:10 -0400

    move transformation 'sandwich' to the minimize function
    Issue: master

--- src/anticlassifier.py
+++ src/anticlassifier.py
@@ -45,8 +45,7 @@ class AntiClassifier(object):
 
         :param num_points: the number of points used to train the anticlassfier
         :type num_points: int
-        """
-   
+        """   
         # generate an initial set of random feature vectors
         rows = [
             {
@@ -64,26 +63,29 @@ class AntiClassifier(object):
         self.anticlassifier.fit(df, p)  
 
     def coefs(self):
-        """Get the coefficients of the anticlassifier's decision function"""
+        """Get the coefficients of the anticlassifier's decision function."""
         return self.anticlassifier.get_params()["logistic"].coef_[0]
 
     def classes(self):
-        """Get the class labels of the anticlassifier"""
+        """Get the class labels of the anticlassifier."""
         return self.anticlassifier.get_params()["logistic"].classes_
 
-    def decision_function(self, x):
-        """Return the decision function of the anticlassifier evaluated at x"""
+    def predict_proba(self, x):
+        """Return the decision function of the anticlassifier evaluated at x.
+         
+        x is not normalized or transformed before computing the gradient. 
+        """
         assert isinstance(x, np.ndarray), "x must be a numpy ndarray"
         return self.anticlassifier.predict_proba(x)[0][0]
 
-    def decision_gradient(self, x):
+    def predict_proba_gradient(self, x):
         """Return the gradient of the anticlassifier decision function 
-        evaluated at x. x must be the raw feature vector, ie not normalized or 
-        transformed.
+        evaluated at x. 
+        
+        x is not normalized or transformed before computing the gradient. 
         """
         assert isinstance(x, np.ndarray), "x must be a numpy ndarray"
         coefs = self.coefs()
-        t = self._transform.transform(x)
         g = [
             # the sigmoid partial derivative
             (c * np.exp(t.dot(coefs))) / ((1.0 + np.exp(t.dot(coefs)))**2.0)
@@ -91,23 +93,28 @@ class AntiClassifier(object):
         ]
         return np.array(g)
 
-    def minimize_decision_function(self, constraints):
+    def minimize_predict_proba(self, constraints):
         """Find the feature vector which minimizes the decision function of the 
-        anticlassifier under constraints. Return this feature vector. """
+        anticlassifier under constraints. Return this feature vector. 
+
+        The anticlassifier performs a normalization on the training data. This
+        function runs the inverse transform on the feature vector before 
+        returning it, i.e. we return the "raw", unnormalized feature vector. 
+        """
 
         # the constraints are expressed as boundaries, not values
         # we're minimizing within a region
  
         x = minimize(
-            self.decision_function, 
-            self.guess(constraints),
+            self.predict_proba, 
+            self._transform.transform(self.guess(constraints)),
             method="SLSQP",
-            jac=self.decision_gradient,
+            jac=self.predict_proba_gradient,
             bounds=[(x["min"], x["max"]) for x in self.feature_specs],
             constraints=constraints,
             tol=0.001
         )
-        return x
+        return self._transform.inverse_transform(x)
 
     def guess(self, constraints):
         """Take an initial guess at the a feature vector under constraints"""
