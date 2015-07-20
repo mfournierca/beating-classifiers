4ca2c0c079cc22c2a01e1b146013da0a9adb660f
Matthew Fournier <fournier.matthew@gmail.com>
2015-07-20 17:57:22 -0400

    add svm classifier
    Issue: master

--- src/classifier.py
+++ src/classifier.py
@@ -1,13 +1,25 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+TRANSFORM = ["scaler", StandardScaler()]
+
 
 def logistic():
-    steps = [
-        ("scaler", StandardScaler()),
-        ("logistic", LogisticRegression())    
-    ]
-    pipe = Pipeline(steps=steps)
-    return pipe
+    return Pipeline(
+        steps=[
+            ("transform", TRANSFORM),
+            ("logistic", LogisticRegression())
+        ]
+    )
 
+
+def svm():
+    return Pipeline(
+        steps=[
+            ("transform", TRANSFORM),
+            ("svm", SVC())
+        ]
+    )
