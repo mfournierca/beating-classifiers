3198abf8ba9ce7e41541ca46654c0a0ba079a9fd
Matthew Fournier <fournier.matthew@gmail.com>
2015-07-23 19:02:54 -0400

    add prep file
    Issue: master

---
+++ src/prep.py
@@ -0,0 +1,7 @@
+import data
+import classifier
+
+xtrain, xtest, ytrain, ytest = data.load_spambase_test_train()
+logistic_model = classifier.logistic().fit(xtrain, ytrain)
+svm_model = classifier.svm().fit(xtrain, ytrain)
+
