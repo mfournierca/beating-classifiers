7f2ae34cd31c62723dc0f6ec94aa9401c7117167
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-09 10:37:20 -0400

    delete prep module
    Issue: master

--- src/prep.py
+++
@@ -1,9 +0,0 @@
-"""Import this module to automatically prepare data and classifiers"""
-
-import data
-import classifier
-
-xtrain, xtest, ytrain, ytest = data.load_spambase_test_train()
-logistic_model = classifier.logistic().fit(xtrain, ytrain)
-svm_model = classifier.svm().fit(xtrain, ytrain)
-
