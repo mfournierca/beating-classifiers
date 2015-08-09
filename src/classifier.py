ade9db4776528bf53ee57564eb6c9340f1d86fe2
Matthew Fournier <fournier.matthew@gmail.com>
2015-08-09 15:42:28 -0400

    add naive_bayes classifier
    Issue: master

--- src/classifier.py
+++ src/classifier.py
@@ -1,10 +1,20 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import StandardScaler
 
 TRANSFORM = FeatureUnion([("scaler", StandardScaler())])
+
+
+def naive_bayes():
+    return Pipeline(
+        steps=[
+            ("transform", TRANSFORM),
+            ("gnb", GaussianNB())
+        ]
+    )
 
 
 def logistic():
